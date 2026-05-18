af340086947dd697c764b208d0a5057a9129f1f6
Grant Nelson <grant.nelson@workiva.com>
2026-05-18 14:36:13 -0600

    Fixing shadowing

--- tests/testdata/shadowing/main.go
+++ tests/testdata/shadowing/main.go
@@ -19,20 +19,24 @@ type EmbedAnother struct{}
 
 func (e *EmbedAnother) Do() string { return `Do it again` }
 
+type ValEmbed struct{}
+
+func (ValEmbed) Do() string { return `Do value` }
+
 // `Container1.Do` shadows `Embedded.Do`.
 type Container1 struct {
 	Embedded
 	Do string
 }
 
-// `Container1.Do` shadows `Embedded.Do`.
+// `Container2.Do` shadows `Embedded.Do`.
 type Container2 struct {
 	*Embedded
 	Do string
 }
 
 // `Embedded.Do` is callable from `*Container3` but not `*Container3.Embedded`
-// since `*Container3.Do` calls with `*Embedded` and `Do` can not be called
+// since `*Container3.Do` calls `Do` with `*Embedded` and `Do` can not be called
 // with a non-pointer to embedded.
 type Container3 struct{ Embedded }
 
@@ -51,7 +55,7 @@ type Container6 struct {
 	Do string
 }
 
-// `Container7.Embedded.Do` and `Container.EmbedAnother.Do` are ambiguous
+// `Container7.Embedded.Do` and `Container7.EmbedAnother.Do` are ambiguous
 // so `Container7.Do` can not be called.
 type Container7 struct {
 	*Embedded
@@ -60,7 +64,7 @@ type Container7 struct {
 
 // Similar `Container7` but checks the ambiguity goes away when the methods
 // are not in contention at the same level of embedding.
-// `Container8.EmbedHolder.EmbedAnother.Do` is deeper than `Container8.Embedded.Do`
+// `Container8.EmbedHolder.EmbedAnother.Do` is "deeper" than `Container8.Embedded.Do`
 // so `Container8.Embedded.Do` is called with `Container.Do`, even through
 // `Container8.EmbedHolder.Do` is also able to be called.
 type (
@@ -77,24 +81,25 @@ type Container9 struct {
 	*Container1
 }
 
-// The method `Do.Embedded.Do` is ambiguous with embedded field name, `Do`.
+// The method `Do` found in the field `Do` (e.g. `Do.Embedded.Do`) is ambiguous
+// with embedded field `Do` itself.
 type (
 	Do          struct{ *Embedded }
 	Container10 struct{ *Do }
 )
 
-// Ambiguity in `Container7` prevents `Embedded.Do` from being ambiguous in
-// `Container11`, so `Container11.Do` will call `Container11.Embedded.Do`.
+// The ambiguity in `Container7` means that `Container7` does not have a `Do`
+// method, meaning `Container11.Do` is not ambiguous and will call `Container11.Embedded.Do`.
 type Container11 struct {
 	*Embedded
 	*Container7
 }
 
 // `Container12.DoerHolder.Doer` and `Container12.Embedded` are ambiguous
-// even though `DoerHolder.Doer` is deeper (see `Container8`), because embedded
+// even though `DoerHolder.Doer` is "deeper" (see `Container8`), because embedded
 // interfaces contribute methods to the embedding interface meaning
 // `DoerHolder` contains a `Do` method thus `Container12.DoerHolder.Do` is at
-// the same depth as `Container12.Embedded.Do`.
+// the same level as `Container12.Embedded.Do`.
 type (
 	DoerHolder  interface{ Doer }
 	Container12 struct {
@@ -103,8 +108,33 @@ type (
 	}
 )
 
-// doIt does a runtime type check to test the preludes, since a compile time
-// type check (e.g. `var _ Doer = ...`) is done in the Go type checker.
+// The field `Do` is a func type so it is callable via `Container13.Do` and
+// will block `Embedded.Do` just like the string in `Container1` does.
+// However `Container13` does not duck-type to `Doer` because `Do` is a field
+// so `Container13.Do` will not be called during `doIt`.
+type Container13 struct {
+	*Embedded
+	Do func() string
+}
+
+// The embedded value must still promote the `Do` method implemented with a value receiver.
+type Container14 struct{ ValEmbed }
+
+// The `Do` field in `WithDoField` and the `Do` method in `Embedded`
+// are at the same level so cause `Do` to be ambiguous in `Container15`.
+// Even though `WithDoField.Do` can be called, since `Do` is a field `WithDoField`
+// will not duck-type to `Doer`.
+type (
+	WithDoField struct{ Do func() string }
+	Container15 struct {
+		*Embedded
+		*WithDoField
+	}
+)
+
+// doIt does a runtime type check to test the `$methodSet` method in the prelude,
+// since a compile time type check (e.g. `var _ Doer = ...`) is done in the
+// Go type checker.
 func doIt(a any) {
 	fmt.Printf("%18T: ", a)
 	if aa, ok := a.(Doer); ok {
@@ -182,4 +212,19 @@ func main() {
 	c12 := &Container12{DoerHolder: &Embedded{}}
 	doIt(c12)
 	doIt(c12.DoerHolder)
+
+	fmt.Println()
+	c13 := &Container13{Do: func() string { return `Don't` }}
+	doIt(c13)
+	doIt(c13.Embedded)
+
+	fmt.Println()
+	c14 := &Container14{}
+	doIt(c14)
+
+	fmt.Println()
+	c15 := &Container15{WithDoField: &WithDoField{Do: func() string { return `Do not try` }}}
+	doIt(c15)
+	doIt(c15.Embedded)
+	doIt(c15.WithDoField)
 }
